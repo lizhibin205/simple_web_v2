1cc54d646fc29870d4d5724012340c30fdeb8006
zhibin02.li <zhibin02.li@vipshop.com>
2019-02-15 22:09:43 +0800

    user

--- simple-service/src/main/java/com/bytrees/service/entity/User.java
+++ simple-service/src/main/java/com/bytrees/service/entity/User.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="goods")
+@Table(name="user")
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
