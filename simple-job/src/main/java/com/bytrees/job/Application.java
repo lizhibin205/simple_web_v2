ad6255bdcdb6ef2b421dda2b5f76d0a114c82cbe
zhibin02.li <zhibin02.li@vipshop.com>
2019-03-13 20:52:57 +0800

    simple job

---
+++ simple-job/src/main/java/com/bytrees/job/Application.java
@@ -0,0 +1,10 @@
+package com.bytrees.job;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] argv) {
+    	System.out.println("job finished.");
+    }
+}
