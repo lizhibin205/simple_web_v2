bd728c01890c43ea07f475ef0dff9a65f01cae63
zhibin02.li <zhibin02.li@vipshop.com>
2019-03-14 18:06:26 +0800

    spring security demo

---
+++ simple-webapp/src/main/java/com/bytrees/web/configuration/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.bytrees.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	//配置用户权限验证规则
+        http
+            .authorizeRequests()
+                .antMatchers("/user", "/user/").authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/user")
+                .permitAll()
+                .and()
+            .logout()
+                .logoutUrl("/user/logout")
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- simple-webapp/src/main/java/com/bytrees/web/controller/UserController.java
+++ simple-webapp/src/main/java/com/bytrees/web/controller/UserController.java
@@ -1,31 +1,26 @@
 package com.bytrees.web.controller;
 
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.bytrees.service.entity.Users;
-import com.bytrees.service.repository.UsersRepository;
-import com.bytrees.web.utils.ResponseJson;
 
-@RestController
+@Controller
+@RequestMapping(value="/user")
 public class UserController {
-	@Autowired
-	private UsersRepository usersRepository;
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user/index");
+		return modelAndView;
+	}
 
-	@RequestMapping(method=RequestMethod.GET, value="/user/{id}")
-    public ResponseEntity<ResponseJson<Users>> index(@PathVariable Long id) {
-		Optional<Users> user = usersRepository.findById(id);
-		if (!user.isPresent()) {
-			return new ResponseEntity<>(new ResponseJson<Users>(404, "user id=" + id + " not found.", null)
-					, HttpStatus.NOT_FOUND);
-			
-		}
-		return new ResponseEntity<>(new ResponseJson<Users>(200, "sucess.", user.get()), HttpStatus.OK);
-    }
+	@RequestMapping(method=RequestMethod.GET, value="/login")
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user/login");
+		return modelAndView;
+	}
 }
