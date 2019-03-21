cb918ec59898ac8789b4687eb03b5b1e19f04422
zhibin02.li <zhibin02.li@vipshop.com>
2019-03-21 16:11:37 +0800

    remove old scan

--- simple-job/src/main/java/com/bytrees/job/Application.java
+++ simple-job/src/main/java/com/bytrees/job/Application.java
@@ -1,10 +1,15 @@
 package com.bytrees.job;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories({"com.bytrees.service.repository"})
+@EntityScan({"com.bytrees.service.entity"})
 public class Application {
     public static void main(String[] argv) {
+    	
     	System.out.println("job finished.");
     }
 }
