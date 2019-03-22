6bb62d0760d365b776eed983bb380c43d543e116
zhibin02.li <zhibin02.li@vipshop.com>
2019-03-22 13:57:04 +0800

    webapp vo use lombok

--- simple-webapp/src/main/java/com/bytrees/web/vo/OrdersVO.java
+++ simple-webapp/src/main/java/com/bytrees/web/vo/OrdersVO.java
@@ -3,30 +3,15 @@ package com.bytrees.web.vo;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class OrdersVO {
     private Long id;
 	private String orderNumber;
 	private Double totalPrice;
     private List<OrderGoodsVO> orderGoodsVOList = new ArrayList<OrderGoodsVO>();
 
-    public Long getId() {
-    	return id;
-    }
-    public void setId(Long id) {
-    	this.id = id;
-    }
-    public String getOrderNumber() {
-    	return orderNumber;
-    }
-    public void setOrderNumber(String orderNumber) {
-    	this.orderNumber = orderNumber;
-    }
-    public Double getTotalPrice() {
-    	return totalPrice;
-    }
-    public void setTotalPrice(Double totalPrice) {
-    	this.totalPrice = totalPrice;
-    }
     public List<OrderGoodsVO> getOrderGoodsList() {
     	return orderGoodsVOList;
     }
